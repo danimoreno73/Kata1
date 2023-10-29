9ab9a9874d7407f203ab04f7b696be249528c3f1
Dani <danielml2003.dml@gmail.com>
2023-10-29 13:43:00 +0000

    Person class created

--- src/software/ulpgc/kata1/Person.java
+++ src/software/ulpgc/kata1/Person.java
@@ -1,4 +1,39 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Person {
+
+    private final String nombre;
+
+    private final LocalDate birthday;
+
+    public Person(String nombre, LocalDate birthday) {
+        this.nombre = nombre;
+        this.birthday = birthday;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+    public int getAge(){
+
+        return toYears(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/365.25);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "nombre='" + nombre + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
 }
