e111f910177ca1609f90c3933a3ea49852f748e6
Dani <danielml2003.dml@gmail.com>
2023-10-29 13:43:56 +0000

    Main class created

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,4 +1,11 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person("Daniel", LocalDate.of(2003,3,14));
+        System.out.println(person.toString());
+    }
 }
